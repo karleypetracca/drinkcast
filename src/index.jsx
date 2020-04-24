44cda91bdfd9a6881a9876ddfe312baf433933ba
infernezor@gmail.com <infernezor@gmail.com>
2020-04-24 10:43:28 -0400

    pulling to test.

--- src/index.jsx
+++ src/index.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import config from './env';
 
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      apiKey={config.API_KEY}
-      sessionId={config.SESSION_ID}
-      token={config.TOKEN}
-    />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
