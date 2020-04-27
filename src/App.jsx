8bed2aa09a7eb7e3b88d76b26354eb24367c4620
Lockett Pundt <kalifornium@hotmail.com>
2020-04-27 11:46:55 -0400

    added background image to App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React, { useReducer } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import { StateProvider } from './context';
 import IndexPage from './components/IndexPage';
 import Reducer from './reducers/Reducer';
@@ -9,6 +10,12 @@ import JoinBar from './components/JoinBar';
 import Bar from './components/Bar';
 import Game from './components/Game';
 import Understood from './components/Understood';
+import wood from './images/wood.jpg';
+
+const AppMain = styled.div`
+  background: url(${wood}) no-repeat top left fixed;
+  background-size: cover;
+`;
 
 const App = () => {
   const initialState = {
@@ -19,16 +26,19 @@ const App = () => {
     userName: 'no user name yet',
   };
 
+
   return (
     <StateProvider value={useReducer(Reducer, initialState)}>
-      <Router>
-        <Route path="/" component={IndexPage} exact />
-        <Route path="/createbar" component={CreateBar} />
-        <Route path="/joinbar" component={JoinBar} />
-        <Route path="/bar" component={Bar} />
-        <Route path="/game" component={Game} />
-        <Route path="/understood" component={Understood} />
-      </Router>
+      <AppMain>
+        <Router>
+          <Route path="/" component={IndexPage} exact />
+          <Route path="/createbar" component={CreateBar} />
+          <Route path="/joinbar" component={JoinBar} />
+          <Route path="/bar" component={Bar} />
+          <Route path="/game" component={Game} />
+          <Route path="/understood" component={Understood} />
+        </Router>
+      </AppMain>
     </StateProvider>
   );
 };
