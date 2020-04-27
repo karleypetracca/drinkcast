ca592138297d299e656bcdbca2632ed85d83aab2
Karley Petracca <karleypetracca@gmail.com>
2020-04-27 16:01:32 -0400

    Feat: implement localStorage to combat refresh wiping Context values

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/prop-types */
 import React, { useState, useContext, useEffect } from 'react';
 import { OTSession, OTStreams, preloadScript } from 'opentok-react';
+import { Redirect } from 'react-router-dom';
 import styled from 'styled-components';
 import ConnectionStatus from './ConnectionStatus';
 import Publisher from './Publisher';
@@ -17,20 +19,36 @@ const BarRoom = styled.div`
   height: var(--main-height);
 `;
 
-const Bar = () => {
+const Bar = ({ match }) => {
   const [value, dispatch] = useContext(StateContext);
   const [error, setError] = useState(false);
   const [connected, setConnected] = useState(false);
+
+  const getLocalData = (localKey) => {
+    const itemStr = localStorage.getItem(localKey);
+    if (!itemStr) {
+      return '';
+    }
+    const item = JSON.parse(itemStr);
+    const currentDate = new Date();
+    if (currentDate.getTime() > item.expiry) {
+      localStorage.removeItem(localKey);
+      return '';
+    }
+    return item.localValue;
+  };
 
   useEffect(() => {
     const loadData = {
       barName: value.barName,
     };
+
     const postURL = `${API_URL}api/updatebar`;
     const loadResp = post(postURL, loadData);
   }, [value.barName]);
 
   console.log('this is the context inside the Bar component: ', value);
+  console.log('this is local: ', getLocalData('barName'));
 
   const sessionEvents = {
     sessionConnected: () => setConnected(true),
@@ -40,28 +58,34 @@ const Bar = () => {
   const onError = (err) => {
     setError(`Failed to connect: ${err.message}`);
   };
+
   const greeting = `Welcome to ${value.barName}! Pull up a seat ${value.userName}!`;
 
   return (
     <>
-      <Nav />
-      <BarRoom>
-        <Modal text={greeting} />
-        <OTSession
-          apiKey={value.key}
-          sessionId={value.sessionId}
-          token={value.token}
-          eventHandlers={sessionEvents}
-          onError={onError}
-        >
-          {/* {error ? <div>error</div> : null} */}
-          {/* <ConnectionStatus connected={connected} /> */}
-          <Publisher />
-          <OTStreams>
-            <Subscriber />
-          </OTStreams>
-        </OTSession>
-      </BarRoom>
+      {
+        !value.barName ? (<Redirect to="/" />)
+          : (
+            <>
+              <Nav />
+              <BarRoom>
+                <Modal text={greeting} />
+                <OTSession
+                  apiKey={value.key || getLocalData('key')}
+                  sessionId={value.sessionId || getLocalData('sessionId')}
+                  token={value.token || getLocalData('token')}
+                  eventHandlers={sessionEvents}
+                  onError={onError}
+                >
+                  <Publisher />
+                  <OTStreams>
+                    <Subscriber />
+                  </OTStreams>
+                </OTSession>
+              </BarRoom>
+            </>
+          )
+      }
     </>
   );
 };
