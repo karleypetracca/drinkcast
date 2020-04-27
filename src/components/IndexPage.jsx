b7843e211c08842edf4d65b3c33b5fbcac84a992
Lockett Pundt <kalifornium@hotmail.com>
2020-04-27 10:20:25 -0400

    no background for the form div. placed the logo in its own div to stack it.

--- src/components/IndexPage.jsx
+++ src/components/IndexPage.jsx
@@ -22,24 +22,24 @@ const IndexDiv = styled.div`
 `;
 
 const IndexBox = styled.div`
-  border-radius: 10px;
-  background-color: var(--orange);
-  width: 50%;
+  border-radius: 5px;
+  width: auto;
   min-width: 300px;
   margin: 15vh auto;
   padding: 1rem;
   height: auto;
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
   text-align: center;
   font-weight: bold;
-
-  @media screen and (max-width: 450px) {
-    
+  
+  .logo {
+    margin: 0 2.3rem;
   }
-
+  @media screen and (max-width: 600px) {
+    flex-direction: column;
+  }
   * {
     margin: 5px auto;
   }
@@ -48,15 +48,19 @@ const IndexBox = styled.div`
 const ButtonBox = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  height: 100%;
+  justify-content: space-evenly;
   margin: .5rem auto;
 `;
 
 const IndexPage = () => (
   <IndexDiv>
     <IndexBox>
-      <Image src={logo} alt="logo" className="inline-logo" />
-      <Image src={tagline} alt="tagline" className="inline-tagline" />
+      <div className="logo">
+        <Image src={logo} alt="logo" className="inline-logo" />
+        <Image src={tagline} alt="tagline" className="inline-tagline" />
+      </div>
+
       <ButtonBox>
         <Button url="/createbar" type="button">CREATE A BAR</Button>
         <p><span>OR</span></p>
