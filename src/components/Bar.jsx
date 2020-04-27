db8a6f3523b2beb7766fec5390f0154cb0e7f59d
Lockett Pundt <kalifornium@hotmail.com>
2020-04-27 16:22:24 -0400

    resolved merge conflict

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -3,6 +3,7 @@ import React, { useState, useContext, useEffect } from 'react';
 import { OTSession, OTStreams, preloadScript } from 'opentok-react';
 import { Redirect } from 'react-router-dom';
 import styled from 'styled-components';
+import ConnectionStatus from './ConnectionStatus';
 import Publisher from './Publisher';
 import Subscriber from './Subscriber';
 import StateContext from '../context';
@@ -11,48 +12,16 @@ import Game from './Game';
 import Nav from './Nav';
 import Modal from './Modal';
 import { post, API_URL } from '../utils/apiConn';
+import wood from '../images/wood.jpg';
 
 
 const BarRoom = styled.div`
   display: flex;
   flex-direction: column;
   height: var(--main-height);
-  text-align: center;
-  
 `;
 
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-const Patrons = styled.div`
-=======
-const Display = styled.div`
->>>>>>> Stashed changes
-  display: flex;
-  flex-wrap: wrap;
-  height: auto;
-  
-  @media screen and (max-width: 600px) {
-    flex-direction: column;
-  }
-`;
-
-const GameDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-content: center;
-  justify-content: center;
-  margin: auto;
-  width: 50%;
-  
-  @media screen and (max-width: 600px) {
-    width: 100%;
-  }
-`;
-
-const Bar = () => {
-=======
 const Bar = ({ match }) => {
->>>>>>> eca396be156d4d1852da5bead6fd764da96b0433
   const [value, dispatch] = useContext(StateContext);
   const [error, setError] = useState(false);
   const [connected, setConnected] = useState(false);
@@ -96,7 +65,6 @@ const Bar = ({ match }) => {
 
   return (
     <>
-<<<<<<< Updated upstream
       {
         !value.barName ? (<Redirect to="/" />)
           : (
@@ -120,32 +88,6 @@ const Bar = ({ match }) => {
             </>
           )
       }
-=======
-      <Nav />
-      <BarRoom>
-        <div className="bar">
-          <h1>{value.barName}</h1>
-        </div>
-        <Modal text={greeting} />
-        <OTSession
-          apiKey={value.key}
-          sessionId={value.sessionId}
-          token={value.token}
-          eventHandlers={sessionEvents}
-          onError={onError}
-        >
-          <Display>
-            <Publisher />
-            <GameDiv>
-              <OTStreams>
-                <Subscriber />
-              </OTStreams>
-              <Game />
-            </GameDiv>
-          </Display>
-        </OTSession>
-      </BarRoom>
->>>>>>> Stashed changes
     </>
   );
 };
