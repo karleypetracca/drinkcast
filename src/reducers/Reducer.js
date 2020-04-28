f74fa42f7bc1655f326e54980f67035947dfcca2
Lockett Pundt <kalifornium@hotmail.com>
2020-04-28 13:06:39 -0400

    added a exit bar action to clear context upon exiting.

--- src/reducers/Reducer.js
+++ src/reducers/Reducer.js
@@ -3,6 +3,14 @@ const Reducer = (state, action) => {
     sessionId, token, barName, userName, key,
   } = action;
   switch (action.type) {
+    case 'ACTION_EXIT_BAR':
+      return {
+        token,
+        sessionId,
+        key,
+        barName,
+        userName,
+      };
     case 'ACTION_CREATE_BAR':
       return {
         ...state,
