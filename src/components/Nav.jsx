5a22a3a6a022a1caf70d0f005c59d80020aa6591
Lockett Pundt <kalifornium@hotmail.com>
2020-04-28 13:06:13 -0400

    added ternary statement to detect whether the client is logged into a bar, whether they are inside the bar component and displaying different items depending on their state and location. added an exit button and click handler to remove the bar info from context and localstorage upon clicking exit.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import styled from 'styled-components';
-
+import StateContext from '../context';
 import Image from './Image';
-
 import logo from '../images/drinkcast-logo-white.png';
-import { get } from '../utils/apiConn';
+import Button from './Button';
 
 const NavStyled = styled.nav`
   width: calc(100vw - (100vw - 100%));
@@ -35,8 +34,28 @@ const NavStyled = styled.nav`
 
 
 const Nav = () => {
+  const location = !!(window.location.href.split('/').pop() === 'bar');
+  const [inBar, setInBar] = useState(location);
+  const [value, dispatch] = useContext(StateContext);
+
+  const clearBarInfo = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: 'ACTION_EXIT_BAR',
+      sessionId: '',
+      token: '',
+      key: '',
+      barName: '',
+      userName: '',
+    });
+
+    localStorage.clear();
+  };
   const getBarName = () => {
-    const item = localStorage.getItem('barName');
+    const item = localStorage.getItem('barName') || '';
+    if (item === '') {
+      return '';
+    }
     const bar = JSON.parse(item);
     return bar.localValue;
   };
@@ -48,14 +67,24 @@ const Nav = () => {
         <Image src={logo} alt="logo" className="nav-logo" />
       </a>
       <div className="links">
-        {localStorage.getItem('sessionId') && localStorage.getItem('token')
+        {localStorage.getItem('sessionId') && localStorage.getItem('token') && !inBar
           ? <a href="/bar">{name}</a> : null}
-        <a href="/joinbar" className="joinBar">
-          JOIN
-        </a>
-        <a href="/createbar" classnam="createBar">
-          CREATE
-        </a>
+        {inBar
+          ? (
+            <Button href="/" type="button" action={(e) => clearBarInfo(e)}>
+              EXIT
+            </Button>
+          )
+          : (
+            <>
+              <a href="/joinbar" className="joinBar">
+                JOIN
+              </a>
+              <a href="/createbar" classnam="createBar">
+                CREATE
+              </a>
+            </>
+          )}
       </div>
 
 
